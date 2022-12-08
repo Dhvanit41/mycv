9fded080322afc73ed56727440c957edca5aae5a
Dhvanit <dhvanitpopat555@gmail.com>
2022-12-08 19:40:02 +0530

    created ticket flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,44 +1,20 @@
-import { Module, ValidationPipe, MiddlewareConsumer } from '@nestjs/common';
-import { APP_PIPE } from '@nestjs/core';
+import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
-import { User } from './users/user.entity';
-import { Report } from './reports/report.entity';
-const cookieSession = require('cookie-session');
-
+import { TicktetModule } from './tickets/ticket.module';
+import { Ticket } from './tickets/ticket.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [User, Report],
+      entities: [Ticket],
       synchronize: true,
     }),
-    UsersModule,
-    ReportsModule,
+    TicktetModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        whitelist: true,
-      }),
-    },
-  ],
+  providers: [AppService],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(
-        cookieSession({
-          keys: ['asdfasfd'],
-        }),
-      )
-      .forRoutes('*');
-  }
-}
+export class AppModule {}
